a326314a84137b6b61899943fd40eb45e4dbc310
michal <michal.k.szydlowski@gmail.com>
2015-11-21 00:08:08 +0100

    Added Winston logging conf file. Code cleanup.

--- models/event.js
+++ models/event.js
@@ -1,2 +1,57 @@
 /* jshint node: true */
-var models = require('models.js');
+var mongoose = require('mongoose');
+var timestamps = require('mongoose-timestamp');
+mongoose.set('debug', true);
+var path = require('path');
+var Models = require(path.join(__dirname,'models.js'));
+var eventTypeList = require(path.join(__dirname, 'enums', 'eventTypeList.js'));
+
+var Schema = mongoose.Schema;
+
+var EventSchema = new Schema({
+	name: {
+		type: String,
+		required: true
+	},
+	eventType: {
+		type: String,
+		required: true,
+		enum: eventTypeList
+	},
+	location: {
+		type: {			
+			geo: {
+				lat: Number,
+				lng: Number
+			}
+		},
+		required: true
+	},
+	eventTime: {
+		type: Date,
+		required: true
+	},
+	host: { 
+		type: Schema.Types.ObjectId, 
+		ref: Models.User,
+		required: true
+	},
+	guests: [{ 
+		type: Schema.Types.ObjectId, 
+		ref: Models.User,
+		required: false
+	}],
+	minParticipants: {
+		type: Number,
+		required: false
+	},
+	maxParticipants: {
+		type: Number,
+		required: false
+	}
+});
+
+EventSchema.plugin(timestamps);
+
+
+module.exports = mongoose.model(Models.Event, EventSchema);
