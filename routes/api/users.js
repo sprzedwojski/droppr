4b49d39a2879e00d3ee53a72966240b2057344ca
michal <michal.k.szydlowski@gmail.com>
2015-11-21 00:56:45 +0100

    Minor fix

--- routes/api/users.js
+++ routes/api/users.js
@@ -16,7 +16,7 @@ var UserModel = require(path.join(__dirname, '..', '..', 'models', 'user.js'));
  */
 router.get('/:id', function(req, res, next) {
     var userId = req.params.id;
-    var user = UserModel.findById(userId, function(err, doc) {
+    UserModel.findById(userId, function(err, doc) {
         if(err) {
             console.log("error finding user by id");
             return next(err);
@@ -24,40 +24,8 @@ router.get('/:id', function(req, res, next) {
 
         res.send(doc);
     });
-
-    //res.send({msg : 'TODO will return details of a user'});
 });
 
-
-// POST =========================================
-
-/**
- * POST User registration.
- */
-/*
-router.post('/', function(req, res, next) {
-    // TODO
-    var name = "Szymon";
-    var surname = "Przedwojski";
-    var email = "szym1000@gmail.com";
-    var pass = "superpass";
-
-    var user = new UserModel();
-    user.name = name;
-    user.surname = surname;
-    user.email = email;
-    user.password = pass;
-    user.save(function(err) {
-        if(err) {
-            // TODO handle error
-            console.log("error saving user to db");
-            return next(err);
-        }
-
-        res.send({msg : 'User registered.'});
-    });
-});
-*/
 
 
 // PUT ==========================================
