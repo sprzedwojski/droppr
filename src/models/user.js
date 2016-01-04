db0acc917ac0922deda73bf0a2dbc76c87f33521
Szymon <szym1000@gmail.com>
2016-01-04 07:54:32 +0100

    Implemented Google token-based user registration.

--- src/models/user.js
+++ src/models/user.js
@@ -4,7 +4,7 @@ var timestamps = require('mongoose-timestamp');
 mongoose.set('debug', true);
 var path = require('path');
 var Models = require(path.join(__dirname,'models.js'));
-var rankList = require(path.join(__dirname, 'enums', 'rankList.js'));
+
 
 var Schema = mongoose.Schema;
 
@@ -17,50 +17,20 @@ var UserSchema = new Schema({
 		type: String,
 		required: true
 	},
-	authentications: {
-		type:{
-			local: {
-				email: String,
-				password: String
-			},
-			google: {
-				email: String,
-				userId: String
-			}
-		},
+	email: {
+		type: String,
 		required: true
 	},
-	birthday: {
-		type: Date,
-		required: false
-	},
-	city: {
-		type: String,
-		required: false
-	},
-	gender: {
-		type: String,
-		required: false,
-		enum: ['male', 'female']
-	},
-	rating: {
-		type: Number,
-		required: false
-	},
-	lastLogin: {
-		type: Date,
-		required: false
-	},
-	rank: {
-		type: String,
-		required: false,
-		enum: rankList
-	},
-	friends: [{
-		type: Schema.Types.ObjectId,
-		ref: Models.User,
-		required: false
-	}]
+    authentications: {
+        local: {
+            email: String,
+            password: String
+        },
+        google: {
+            email: String,
+            userId: String
+        }
+    }
 });
 
 UserSchema.plugin(timestamps);
--- src/routes/api/userRegister.js
+++ src/routes/api/userRegister.js
@@ -9,54 +9,47 @@
 var path = require('path');
 var express = require('express');
 var router = express.Router();
-var UserModel = require(path.join(__dirname, '..', '..', 'models', 'user.js'));
 var logger = require(path.join(__dirname, '..', '..','utils', 'logger.js'));
 var statusCodes = require('http-status-codes');
+var userUtils = require(path.join(__dirname, '..', 'utils', 'userUtils.js'));
+var authMethods = ["email", "google"];
 
 /**
  * POST User registration.
  */
-router.post('/', function(req, res, next) {
+router.post('/', function(req,res,next) {
     logger.info("Inside POST User registration.");
     logger.info(req.body);
+    logger.info("index: " + authMethods.indexOf(req.body.authMethod));
 
-    var name = req.body.name;
-    var surname = req.body.surname;
-    var email = req.body.email;
-    var pass = req.body.passwordHash;
+    // Check if the authentication method is specified
+    if(authMethods.indexOf(req.body.authMethod) < 0) {
+        return res.status(400)
+            .json({msg:"Wrong authentication method provided. Must be one of the following: [" + authMethods + "]"});
+    }
 
-    UserModel.findOne({email:email}, function(err, doc) {
-         if(err) {
-             logger.error("Problem while finding user.");
-             return next(err);
-         }
-
-        if(doc) {
-            logger.error("User email already taken.");
-            return res.status(statusCodes.UNAUTHORIZED).json({msg:"User email already taken."});
-        }
-
-        var user = new UserModel();
-        user.name = name;
-        user.surname = surname;
-        user.authentications = {
-            local:{
-                email: email,
-                password: pass
-            }
-        };
-        user.save(function(err, doc) {
-            if(err) {
-                logger.error("Error saving user to db. Possibly required fields missing.");
+    if(req.body.authMethod == "google") {
+        userUtils.registerGoogleUser(req.body.name, req.body.surname, req.body.token, function (err, user) {
+            if (err) {
                 return next(err);
             }
 
             // Returning the created user
             logger.debug("New user created: " + doc);
-            return res.status(statusCodes.CREATED).json(doc);
+            return res.status(statusCodes.CREATED).json(user);
         });
+    } else {
+        userUtils.registerUser(req.body.name, req.body.surname, req.body.email, req.body.passwordHash, function (err, user) {
+            if (err) {
+                return next(err);
+            }
+            if (user === null) {
+                return res.status(401).json({msg: "User email already taken."});
+            }
 
-    });
+            return res.status(201).send(user);
+        });
+    }
 
 });
 
